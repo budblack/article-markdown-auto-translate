81b28c7770039c90b4bdbcd743de69f888978b90
budblack <budblack@qq.com>
2024-07-01 15:33:09 +0000

    chore: Update OpenAI translation functionality and error handling in index.ts

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import { getInput } from '@actions/core';
 import { context } from '@actions/github';
 import { GitHub } from '@actions/github/lib/utils';
 
-import { translateWithOpenAI } from 'openai-translate';
+import OpenAI from 'openai';
 
 async function main() {
   // 打印仓库信息，包括仓库名、分支名、提交ID
@@ -19,19 +19,18 @@ async function main() {
     });
   }
 
-  const OPENAI_API_KEY = getInput('openaiApiKey');
-  const message = 'hello world';
-  const openai_url = 'https://api.openai.com/v1/chat/completions';
-  const model = 'gpt-3.5-turbo';
-  const target_langualge = 'zh-cn';
-
-  // console.log('OPENAI_API_KEY:', OPENAI_API_KEY);
-  console.log('OPENAI_API_KEY len:', OPENAI_API_KEY.length);
-  // console.log('OPENAI_API_KEY arr:', `${OPENAI_API_KEY}`.split('').join(',') );
 
   try {
-    const result = await translateWithOpenAI(OPENAI_API_KEY, openai_url, model, message, target_langualge);
-    console.log('result:', result);
+    const OPENAI_API_KEY = getInput('openaiApiKey');
+    const openai = new OpenAI({      apiKey: process.env['OPENAI_API_KEY']});
+
+    const params: OpenAI.Chat.ChatCompletionCreateParams = {
+      messages: [{ role: 'user', content: 'Say this is a test' }],
+      model: 'gpt-3.5-turbo',
+    };
+    const chatCompletion: OpenAI.Chat.ChatCompletion = await openai.chat.completions.create(params);
+
+    console.log('chatCompletion:', chatCompletion);
   } catch (error) {
     console.error('error:', error);
   }
