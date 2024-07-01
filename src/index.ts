389f120cb9516e52635867f82f3b6ff44d360e59
budblack <budblack@qq.com>
2024-07-01 15:57:00 +0000

    chore: Update OpenAI translation functionality and error handling in index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,39 +1,50 @@
 import { getInput } from '@actions/core';
 import { context } from '@actions/github';
 import { GitHub } from '@actions/github/lib/utils';
+import { access, constants, readFile } from 'fs-extra'
+import { marked } from 'marked';
+import { parseHTML } from 'linkedom';
 
 import OpenAI from 'openai';
+import { join } from 'path';
+
+function getRouteAddr(markdown: string) {
+  const IndexHTML = /index\.\w+$/i;
+  const { document } = parseHTML(marked(markdown));
+
+  const { href } = document.querySelector('a') || {};
+
+  const URI = new URL(href);
+
+  URI.pathname = URI.pathname.replace(IndexHTML, '');
+
+  return URI + '';
+}
 
 async function main() {
-  // 打印仓库信息，包括仓库名、分支名、提交ID
-  console.log('Repository:', context.repo.repo);
-  console.log('Branch:', context.ref);
-  console.log('Commit:', context.sha);
+  const newsLink = getInput('newsLink'),
+    ignoreSelector = getInput('ignoreSelector'),
+    markDownFilePath = getInput('markDownFilePath') || './';
 
-  // 检索文件，打印变更文件列表
-  const files = context.payload.pull_request?.files;
-  if (files) {
-    console.log('Files:');
-    files.forEach((file: any) => {
-      console.log(file.filename);
-    });
-  }
+  const path = getRouteAddr(newsLink);
+  const filePath = join(
+    markDownFilePath,
+    path.split('/').filter(Boolean).at(-1) + '.md'
+  );
 
+  const str_md = await readFile(filePath, 'utf-8');
+  const str_prompt = `请将下文翻译为中文，并保留完整排版。\n`
 
-  try {
-    const OPENAI_API_KEY = getInput('openaiApiKey');
-    const openai = new OpenAI({      apiKey: process.env['OPENAI_API_KEY']});
+  const OPENAI_API_KEY = getInput('openaiApiKey');
+  const openai = new OpenAI({ apiKey: OPENAI_API_KEY });
 
-    const params: OpenAI.Chat.ChatCompletionCreateParams = {
-      messages: [{ role: 'user', content: 'Say this is a test' }],
-      model: 'gpt-3.5-turbo',
-    };
-    const chatCompletion: OpenAI.Chat.ChatCompletion = await openai.chat.completions.create(params);
-
-    console.log('chatCompletion:', chatCompletion);
-  } catch (error) {
-    console.error('error:', error);
-  }
+  const params: OpenAI.Chat.ChatCompletionCreateParams = {
+    messages: [{ role: 'user', content: str_prompt + str_md }],
+    model: 'gpt-3.5-turbo',
+  };
+  const chatCompletion: OpenAI.Chat.ChatCompletion = await openai.chat.completions.create(params);
+  const response = chatCompletion.choices[0].message.content;
+  console.log('response:', response);
 }
 
 main()
