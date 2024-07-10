ddebe5c6b11f608b68695e92ba292ae626d0a0ea
budblack <budblack@qq.com>
2024-07-10 12:26:24 +0000

    chore: Refactor OpenAI translation functionality and improve error handling in index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,18 @@
 import { getInput } from '@actions/core';
 import { context } from '@actions/github';
 import { GitHub } from '@actions/github/lib/utils';
-import { access, constants, readFile, unlink, writeFile } from 'fs-extra'
+import { access, constants, ensureDir, ensureFile, readFile, unlink, writeFile } from 'fs-extra'
 import { marked } from 'marked';
 import { parseHTML } from 'linkedom';
 
 import OpenAI from 'openai';
 import { join } from 'path';
+import { assert } from 'console';
+
+const map_str_prompts: any = {
+  "zh-cn": "我有段 md 文件，请翻译为中文。翻译需要严格保留源文件 markdown 排版布局，请直接输出，不要在作询问。\n",
+  "ja-jp": "私はmdファイルを持っています、それを中国語に翻訳してください。翻訳は厳密に元のファイルのmarkdownレイアウトを保持する必要があります。直接出力してください、質問をしないでください。\n",
+}
 
 function getRouteAddr(markdown: string) {
   const IndexHTML = /index\.\w+$/i;
@@ -21,10 +27,12 @@ function getRouteAddr(markdown: string) {
   return URI + '';
 }
 
-async function translate(str_md: string) {
-  const str_prompt = `我有段 md 文件，请翻译为中文。翻译需要严格保留源文件 markdown 排版布局，请直接输出，不要在作询问。\n`
+async function translate(str_md: string, str_prompt: string) {
+
+
 
   const OPENAI_API_KEY = getInput('openaiApiKey');
+
   const openai = new OpenAI({ apiKey: OPENAI_API_KEY });
 
   const params: OpenAI.Chat.ChatCompletionCreateParams = {
@@ -33,7 +41,6 @@ async function translate(str_md: string) {
   };
   const chatCompletion: OpenAI.Chat.ChatCompletion = await openai.chat.completions.create(params);
   const response = chatCompletion.choices[0].message.content;
-
 
 
   console.log(str_md);
@@ -46,15 +53,31 @@ async function translate(str_md: string) {
 async function main() {
   const newsLink = getInput('newsLink'),
     ignoreSelector = getInput('ignoreSelector'),
-    markDownFilePath = getInput('markDownFilePath') || './';
+    input_mdfile_dir = getInput('markDownFilePath') || './';
+
+  // 从 issue 中获取 title
+  const str_issue_title = context.payload.issue?.title || '';
+  // [Auto][zh-cn]（此处替换为翻译的中文标题）
+  const target_language = str_issue_title.match(/\[Auto\]\[(\w{5})\]/)?.[0] || '';
+
+  const str_prompt = map_str_prompts[target_language];
+  assert(str_prompt, 'targetLanguage is not supported!');
+
+  // markDownFilePath = './articles/raw/';
+  // 根据语言生成不同的目标文件夹
+  const output_mdfile_dir = join(input_mdfile_dir, '..', target_language);
 
   const path = getRouteAddr(newsLink);
-  const filePath = join(
-    markDownFilePath,
+  const input_mdfile_path = join(
+    input_mdfile_dir,
+    path.split('/').filter(Boolean).at(-1) + '.md'
+  );
+  const output_mdfile_path = join(
+    output_mdfile_dir,
     path.split('/').filter(Boolean).at(-1) + '.md'
   );
 
-  const str_md = await readFile(filePath, 'utf-8');
+  const str_md = await readFile(input_mdfile_path, 'utf-8');
 
   const arr_str_md = str_md.split('\n\n');
   let str_md_translated = '';
@@ -67,11 +90,12 @@ async function main() {
     // 快速扫描 str 中有多少个 ``` 符号
     const count = (str.match(/```/g) || []).length;
     count_scope_token += count;
+
     // 如果是代码块，直接输出
     if (count_scope_token % 2 === 1) {
       str_temp += (str + '\n\n');
       continue;
-    }else{
+    } else {
       count_scope_token = 0;
     }
 
@@ -80,7 +104,7 @@ async function main() {
       continue;
     }
     console.log(`============== [${i} / ${len}] ==============`)
-    const str_translated = await translate(str_temp);
+    const str_translated = await translate(str_temp, str_prompt);
     str_temp = '';
 
     str_md_translated += str_translated + '\n\n';
@@ -88,9 +112,10 @@ async function main() {
   }
 
   // 写文件
-  console.log('filePath:', filePath);
-  await unlink(filePath);
-  await writeFile(filePath, str_md_translated);
+  console.log('output_mdfile_path:', output_mdfile_path);
+  await ensureFile(output_mdfile_path);
+  await unlink(output_mdfile_path);
+  await writeFile(output_mdfile_path, str_md_translated);
 }
 
 main()
